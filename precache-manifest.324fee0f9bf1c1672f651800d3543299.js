661d0ed998aab633511ebdb53eda7e5298c73525
miaogang1 <miaogang@jd.com>
2019-09-10 19:15:43 +0800

    ccc

---
+++ precache-manifest.324fee0f9bf1c1672f651800d3543299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99f7047b6e369288b54cc7a1f37972a6",
+    "url": "//mragon1997.github.io/nn/index.html"
+  },
+  {
+    "revision": "737de0ed8c6c6f26f652",
+    "url": "//mragon1997.github.io/nn/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1c026340af6ac9c37519",
+    "url": "//mragon1997.github.io/nn/static/js/2.a05432c8.chunk.js"
+  },
+  {
+    "revision": "737de0ed8c6c6f26f652",
+    "url": "//mragon1997.github.io/nn/static/js/main.9ae6f9d7.chunk.js"
+  },
+  {
+    "revision": "cb6fcf567274b02259fa",
+    "url": "//mragon1997.github.io/nn/static/js/runtime~main.ad4d0617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//mragon1997.github.io/nn/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09cabc61ca110a2254796bd838e413a6.js"
+  "//mragon1997.github.io/nn/precache-manifest.324fee0f9bf1c1672f651800d3543299.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//mragon1997.github.io/nn/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
